6ac848cd68abb7b26b568d31930c0c652bb5f7cc
pedromonteiro01 <pmapm@ua.pt>
2022-05-31 16:06:28 +0100

    add home page

--- service/src/App.js
+++ service/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Login from './Views/Login/Login';
 import SignUp from './Views/SignUp/SignUp';
 import Navbar from './Components/Navbar/Navbar';
+import Home from './Views/Home/Home';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
     <BrowserRouter>
         <Navbar />
           <Switch>
-            <Route path='/login' exact component={Login}/>
+          <Route path='/' exact component={Home}/>
+            <Route path='/login' component={Login}/>
             <Route path='/sign-up' component={SignUp}/>
           </Switch>
       </BrowserRouter>
