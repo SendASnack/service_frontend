485184b049666275f054f6f1d088f1282b4d9938
rezeett <edu_fernandes_2001@live.com.pt>
2022-06-02 11:42:07 +0100

    Features: Login and Register comunicating with backend

--- service/src/App.js
+++ service/src/App.js
@@ -3,18 +3,18 @@ import Login from './Views/Login/Login';
 import SignUp from './Views/SignUp/SignUp';
 import Home from './Views/Home/Home';
 import Profile from './Views/Profile/Profile';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <>
     <BrowserRouter>
-          <Switch>
-          <Route path='/' exact component={Home}/>
-            <Route path='/login' component={Login}/>
-            <Route path='/sign-up' component={SignUp}/>
-            <Route path='/profile' component={Profile}/>
-          </Switch>
+          <Routes>
+          <Route path='/' exact element={<Home/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/sign-up' element={<SignUp/>}/>
+            <Route path='/profile' element={<Profile/>}/>
+          </Routes>
       </BrowserRouter>
     </>
   );
--- service/src/Views/Login/Login.js
+++ service/src/Views/Login/Login.js
@@ -4,6 +4,20 @@ import logo from '../img/signup-image.png';
 import './Login.css';
 
 const Login = () => {
+
+    function handleLogin(){
+        var email = document.getElementById("email");
+        var password = document.getElementById("password");
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({email: email.value, password: password.value })
+        };
+        fetch('http://localhost:8080/api/auth/login', requestOptions)
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+
     return (
         <>
         <div className='login'>
@@ -12,10 +26,10 @@ const Login = () => {
             </div>
             <h2 className='login-header'>Login</h2>
             <form className='input-form'>
-                <Input type="email" label="Email" value="email" />
-                <Input type="password" label="Password" value="password" />
+                <Input id="email" type="email" label="Email" value="email" />
+                <Input id="password" type="password" label="Password" value="password" />
             </form>
-            <div className='sign-up-button'>
+            <div className='sign-up-button' onClick={handleLogin}>
                 <Button type="submit" class="sign-up" buttonText="Login"/>
             </div>
         </div>
