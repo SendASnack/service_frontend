d0f60c5c0716d47e884382c50bcac456e659a77b
pedromonteiro01 <pmapm@ua.pt>
2022-06-07 23:51:32 +0100

    information showing on profile

--- service/src/Views/Login/Login.js
+++ service/src/Views/Login/Login.js
@@ -3,34 +3,40 @@ import { useNavigate, useLocation } from "react-router-dom";
 import Button from '../../Components/Button/Button';
 import Input from '../../Components/Input/Input';
 import logo from '../img/signup-image.png';
+import {toast} from "react-toastify";
+import axios from "axios";
 import './Login.css';
 
 const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const [token, setToken] = useState(null);
     const navigate = useNavigate();
     const location = useLocation();
 
+    const notify = (message) => toast(message);
+
     function handleLogin(){
-        var email = document.getElementById("email");
-        var password = document.getElementById("password");
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({email: email.value, password: password.value })
-        };
-        fetch('http://localhost:8080/api/auth/login', requestOptions)
-            .then(response => response.json())
-            .then(data => {
-                let arr = [];
-                console.log(data);
-                arr.push(data);
-                arr.map((info) => {
-                    if (info.message=="Authentication succeeded."){
-                        setToken(info.token)
-                        navigate('/', {token: token});
-                    }
-                });
+        if (!email || !password){
+            console.log(email)
+            console.log(password)
+            notify("Please fill all fields");
+            return;
+        }
+
+        axios.post('http://localhost:8080/api/auth/login', {
+            email: email,
+            password: password
+        })
+            .then((response) => {
+                notify(response.data.message);
+                localStorage.setItem('token', response.data.token);
+                setTimeout(() => window.location.replace("/"), 2000);
+            }, (error) => {
+                notify(JSON.parse(error.request.response)['message']);
+                console.log(error);
             });
     }
 
@@ -42,8 +48,8 @@ const Login = () => {
             </div>
             <h2 className='login-header'>Login</h2>
             <form className='input-form'>
-                <Input id="email" type="email" label="Email" value="email" />
-                <Input id="password" type="password" label="Password" value="password" />
+                <Input id="email" type="email" label="Email" value="email" on_value_changed={setEmail} />
+                <Input id="password" type="password" label="Password" value="password" on_value_changed={setPassword} />
             </form>
             <div className='sign-up-button' onClick={handleLogin}>
                 <Button type="submit" class="sign-up" buttonText="Login"/>
