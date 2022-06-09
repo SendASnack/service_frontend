e7b53016bb0f4b139dc53dacadfd7358ad89f6f1
pedromonteiro01 <pmapm@ua.pt>
2022-06-09 15:43:49 +0100

    add icon button to navbar

--- service/src/Components/IconButton/IconButton.js
+++ service/src/Components/IconButton/IconButton.js
@@ -1,9 +1,20 @@
-import './IconButton.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import "./IconButton.css";
 
 const IconButton = (props) => {
-    return (
-        <button className={`icon-button ${props.class}`} type={props.type}><i className={props.icon}>X</i></button>
-    );
-}
+  const navigate = useNavigate();
+
+  const handleRoute = () => {
+    navigate(props.path);
+  };
+
+  return (
+    <button className={`icon-button ${props.class}`} type={props.type} onClick={handleRoute}>
+      <FontAwesomeIcon icon={faShoppingCart} />
+    </button>
+  );
+};
 
 export default IconButton;
