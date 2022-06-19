6126064e674625c77592276890b2f06c7980dbdc
pedromonteiro01 <pmapm@ua.pt>
2022-06-19 14:35:51 +0100

    get products from db

--- service/src/Views/Home/Components/HomeContent.js
+++ service/src/Views/Home/Components/HomeContent.js
@@ -1,9 +1,34 @@
 import BurgerCard from "../../../Components/BurgerCard/BurgerCard";
 import ItemShape from "../../../Components/ItemShape/ItemShape";
 import VerticalText from "../../../Components/Text/VerticalText/VerticalText";
+import { useState ,useEffect } from "react";
 import './HomeContent.css';
 
 const HomeContent = () => {
+
+    const [info, setInfo] = useState(null);
+
+    useEffect(() => {
+        let result = [];
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Authorization': 'Bearer ' + localStorage.getItem("token")},
+        };
+        fetch('http://localhost:8080/api/products?category=“”&maxPrice=“”', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                let arr = [];
+                let i = 0;
+                arr.push(data);
+                arr.map((product) => {
+                    result.push(<BurgerCard key={product[i].id} title={product[i].name} text={product[i].description} />);
+                    i++;
+                })
+                setInfo(result);
+            });
+    }, []);
+
     return (
         <>
             <div className="home-content" data-testid="home-content">  
@@ -11,11 +36,7 @@ const HomeContent = () => {
                 <h2 className="home-content-title">Dine In + Delivery</h2>
                 <VerticalText class="middle dark" text="Menu" />
                 <div className="products-cards">
-                    <BurgerCard title="Burger Thief" text="Lorem ipsum dolor sit amet, consecture" />
-                    <BurgerCard title="Burger Thief" text="Lorem ipsum dolor sit amet, consecture" />
-                    <BurgerCard title="Burger Thief" text="Lorem ipsum dolor sit amet, consecture" />
-                    <BurgerCard title="Burger Thief" text="Lorem ipsum dolor sit amet, consecture" />
-                    <BurgerCard title="Burger Thief" text="Lorem ipsum dolor sit amet, consecture" />
+                    {info!==null ? info : null}
                 </div>
             </div>
             <div className="item-shapes">
