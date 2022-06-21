b667485e371c7a775028065755c73960a6c5adaa
pedromonteiro01 <pmapm@ua.pt>
2022-06-21 15:42:04 +0100

    fix get products from api

--- service/src/Views/Home/Components/HomeContent.js
+++ service/src/Views/Home/Components/HomeContent.js
@@ -5,9 +5,7 @@ import { useState ,useEffect } from "react";
 import './HomeContent.css';
 
 const HomeContent = () => {
-
-    const [info, setInfo] = useState([]);
-    const [result, setResult] = useState([]);
+    const [result, setResult] = useState(null);
 
     useEffect(() => {
         let array = [];
@@ -18,15 +16,13 @@ const HomeContent = () => {
         fetch('http://localhost:8080/api/products?category=&maxPrice=', requestOptions)
             .then(response => response.json())
             .then(data => {
-                setInfo(data);
+                data.map((product) => {
+                    array.push(
+                        <BurgerCard key={product.id} title={product.name} text={product.description} />
+                    )
+                })
+                setResult(array);
             });
-
-            info.map((product) => {
-                array.push(
-                    <BurgerCard key={product.id} title={product.name} text={product.description} />
-                )
-            })
-            setResult(array);
     }, []);
 
     return (
